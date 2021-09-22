3b2224064bd410a403c21c7593a06c600f8dd084
Caitie Boeyens <caitie@bigbrave.co.za>
2021-09-22 17:24:58 +0200

    [F] added the changelog and other files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,14 @@ import "../styles/globals.css";
 import "../components/elements/icons/library";
 import "tailwindcss/tailwind.css";
 import "@fortawesome/fontawesome/styles.css";
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import DataProvider from "../context/data";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <DataProvider>
+      <Component {...pageProps} />
+    </DataProvider>
+  );
 }
-export default MyApp
+export default MyApp;
